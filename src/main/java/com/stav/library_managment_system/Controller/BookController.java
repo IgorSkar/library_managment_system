8e2c94cef1ce2c34970967d2cdc32e6e15d844a7
sahrabile <sahra.bile@educ.newton.se>
2022-05-09 16:09:31 +0200

    Sahra,Liam,igor,Anuka parkodade tillsammans för det mesta

--- src/main/java/com/stav/library_managment_system/Controller/BookController.java
+++ src/main/java/com/stav/library_managment_system/Controller/BookController.java
@@ -66,6 +66,12 @@ public class BookController {
         }
         return new ResponseEntity<String>("book added successfully!", HttpStatus.OK);
     }
+    @DeleteMapping("/{bookId}")
+
+    public ResponseEntity<?> deleteBookById(@PathVariable int bookId){
+        int result = bookDAO.deleteBook(bookId);
+        return new ResponseEntity<String>("book deleted successfully!",HttpStatus.OK);
+    }
 }
 
 
